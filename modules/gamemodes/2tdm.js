416d40750aa52f099d04bd0b80c5ab784f01e54f
Andrew <74645668+andrew-spec@users.noreply.github.com>
2022-07-23 14:32:15 +0100

    Create 2tdm.js

---
+++ modules/gamemodes/2tdm.js
@@ -0,0 +1,21 @@
+exports.tdm2 = {
+  NAME: "2TDM",
+  WIDTH: 6000,
+  HEIGHT: 6000,
+  X_GRID: 9,
+  Y_GRID: 9,
+  BOTS: 5,
+  MAX_FOOD: 50,
+  TEAMS: 2,
+  ROOM_SETUP: [
+    ["bas1", "obsr", "obsr", "obsr", "obsr", "obsr", "obsr", "obsr", "bas2"],
+    ["bap1", "arcl", "obsr", "obsr", "arc2", "obsr", "obsr", "arcl", "bap2"],
+    ["bas1", "obsr", "obsr", "obsr", "roid", "obsr", "obsr", "obsr", "bas2"],
+    ["bap1", "obsr", "obsr", "nest", "nest", "nest", "obsr", "obsr", "bap2"],
+    ["bas1", "arc2", "roid", "nest", "nest", "nest", "roid", "arc2", "bas2"],
+    ["bap1", "obsr", "obsr", "nest", "nest", "nest", "obsr", "obsr", "bap2"],
+    ["bas1", "obsr", "obsr", "obsr", "roid", "obsr", "obsr", "obsr", "bas2"],
+    ["bap1", "arcl", "obsr", "obsr", "arc2", "obsr", "obsr", "arcl", "bap2"],
+    ["bas1", "obsr", "obsr", "obsr", "obsr", "obsr", "obsr", "obsr", "bas2"],
+  ]
+}
